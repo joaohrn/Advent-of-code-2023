a1273a22db91fd87f119f3931344c4cb4c348b5e
joaohrn0 <joaohrn0@gmail.com>
2024-08-16 21:21:17 -0300

    Day2 Complete

---
+++ Day2/Part1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml.XPath;
+
+StreamReader sr = new StreamReader("./../Inputs.txt");
+var line = sr.ReadLine();
+IEnumerable<int> AllGames = [];
+IEnumerable<int> BadGames = [];
+while (line != null)
+{
+    int GameID = int.Parse(line.Split(':')[0].Split(' ')[1]);
+    AllGames = AllGames.Append(GameID);
+    string[] Results = string.Join(',', line.Split(':')[1].Split(';')).Split(',');
+    foreach (string result in Results)
+    {
+        string[] game = result.Trim().Split(' ');
+        if (CheckGameIsBad(game))
+        {
+            BadGames = BadGames.Append(GameID);
+            break;
+        }
+    }
+    line = sr.ReadLine();
+}
+
+sr.Close();
+Console.WriteLine(string.Join(',', BadGames));
+
+int sum = 0;
+foreach (int game in AllGames)
+{
+    if (!BadGames.Contains(game))
+        sum += game;
+}
+
+
+Console.WriteLine(sum);
+
+bool CheckGameIsBad(string[] game)
+{
+    if (game[1] == "red")
+    {
+        return int.Parse(game[0]) > 12;
+    }
+    else if (game[1] == "green")
+    {
+        return int.Parse(game[0]) > 13;
+    }
+    else
+    {
+        return int.Parse(game[0]) > 14;
+    }
+}
---
+++ Day2/Part2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.XPath;
+
+StreamReader sr = new StreamReader("./../Inputs.txt");
+var line = sr.ReadLine();
+IEnumerable<int> Powers = [];
+while (line != null)
+{
+    int GameID = int.Parse(line.Split(':')[0].Split(' ')[1]);
+    string[] Results = string.Join(',', line.Split(':')[1].Split(';')).Split(',');
+    int redMin = 0;
+    int blueMin = 0;
+    int greenMin = 0;
+    foreach (string result in Results)
+    {
+        string[] game = result.Trim().Split(" ");
+        if (game[1] == "red" && int.Parse(game[0]) > redMin)
+        {
+            redMin = int.Parse(game[0]);
+        }
+        else if (game[1] == "green" && int.Parse(game[0]) > greenMin)
+        {
+            greenMin = int.Parse(game[0]);
+        }
+        else if (game[1] == "blue" && int.Parse(game[0]) > blueMin)
+        {
+            blueMin = int.Parse(game[0]);
+        }
+    }
+    Powers = Powers.Append(redMin * greenMin * blueMin);
+    line = sr.ReadLine();
+}
+
+sr.Close();
+
+int sum = 0;
+foreach (int Power in Powers)
+{
+    sum += Power;
+}
+
+Console.WriteLine(sum);
